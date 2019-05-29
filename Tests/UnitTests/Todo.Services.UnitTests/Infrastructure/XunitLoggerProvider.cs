9eb7674152032f23a126026781f6746601201298
Bogdan Marian <satrapu@users.noreply.github.com>
2019-05-29 23:11:41 +0300

    Add back class

---
+++ Tests/UnitTests/Todo.Services.UnitTests/Infrastructure/XunitLoggerProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+using Xunit.Abstractions;
+
+namespace Todo.Services.Infrastructure
+{
+    /// <summary>
+    /// https://stackoverflow.com/a/46172875
+    /// </summary>
+    public class XunitLoggerProvider : ILoggerProvider
+    {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public XunitLoggerProvider(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new XunitLogger(testOutputHelper, categoryName);
+        }
+
+        public ILogger CreateLogger<T>() where T: class
+        {
+            return new XunitLogger(testOutputHelper, typeof(T).FullName);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
