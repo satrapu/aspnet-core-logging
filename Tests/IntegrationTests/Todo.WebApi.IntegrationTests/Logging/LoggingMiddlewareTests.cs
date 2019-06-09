c8736f587cf3b93bb1f3d82bd050559f58c27798
Bogdan Marian <satrapu@users.noreply.github.com>
2019-06-09 22:25:33 +0300

    Add integration tests covering logging middleware

---
+++ Tests/IntegrationTests/Todo.WebApi.IntegrationTests/Logging/LoggingMiddlewareTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Todo.WebApi.Infrastructure;
+
+namespace Todo.WebApi.Logging
+{
+    /// <summary>
+    /// Contains integration tests targeting <see cref="LoggingMiddleware"/> class.
+    /// </summary>
+    [TestFixture]
+    public class LoggingMiddlewareTests
+    {
+        private TodoWebApplicationFactory testFactory;
+
+        [OneTimeSetUp]
+        public void GivenAnHttpRequestIsToBePerformed()
+        {
+            testFactory = new TodoWebApplicationFactory();
+        }
+
+        /// <summary>
+        /// Tests <see cref="LoggingMiddleware.Invoke"/> method.
+        /// </summary>
+        [Test]
+        public async Task Invoke_WithTextHeaderNotTriggeringRequestBeingLogged_MustSucceed()
+        {
+            // Arrange
+            using (var client = testFactory.CreateClient())
+            {
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "api/todo");
+                httpRequestMessage.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("qwerty/ytrewq"));
+
+                // Act
+                var httpResponseMessage = await client.SendAsync(httpRequestMessage).ConfigureAwait(false);
+
+                // Assert
+                httpResponseMessage.IsSuccessStatusCode.Should().BeFalse();
+            }
+        }
+
+        /// <summary>
+        /// Tests <see cref="LoggingMiddleware.Invoke"/> method.
+        /// </summary>
+        [Test]
+        public async Task Invoke_WithTextHeaderTriggeringRequestBeingLogged_MustSucceed()
+        {
+            // Arrange
+            using (var client = testFactory.CreateClient())
+            {
+                var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "api/todo");
+                httpRequestMessage.Headers.Accept.Add(MediaTypeWithQualityHeaderValue.Parse("application/json"));
+
+                // Act
+                var httpResponseMessage = await client.SendAsync(httpRequestMessage).ConfigureAwait(false);
+
+                // Assert
+                httpResponseMessage.IsSuccessStatusCode.Should().BeTrue();
+            }
+        }
+    }
+}
