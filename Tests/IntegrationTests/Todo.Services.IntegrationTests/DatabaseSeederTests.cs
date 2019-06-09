5152a87b73261fedfbd5e75637387250aeb86836
Bogdan Marian <satrapu@users.noreply.github.com>
2019-06-09 17:22:17 +0300

    Add integration tests

---
+++ Tests/IntegrationTests/Todo.Services.IntegrationTests/DatabaseSeederTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using Todo.Persistence;
+
+namespace Todo.Services
+{
+    /// <summary>
+    /// Contains integration tests targeting <see cref="DatabaseSeeder"/> class.
+    /// </summary>
+    [TestFixture]
+    public class DatabaseSeederTests
+    {
+        /// <summary>
+        /// Tests <see cref="DatabaseSeeder.Seed"/> method.
+        /// </summary>
+        [Test]
+        public void Seed_UsingValidArguments_MustReturnTrue()
+        {
+            // Arrange
+            var dbName = Guid.NewGuid().ToString();
+            var dbContextOptions = new DbContextOptionsBuilder<TodoDbContext>().UseInMemoryDatabase(dbName).Options;
+
+            using (var todoDbContext = new TodoDbContext(dbContextOptions))
+            {
+                var loggerFactory = new NullLoggerFactory();
+                var logger = loggerFactory.CreateLogger<DatabaseSeeder>();
+                var databaseSeeder = new DatabaseSeeder(logger);
+                var seedingData = Enumerable.Empty<TodoItem>();
+
+                // Act 
+                var hasSeededSucceeded = databaseSeeder.Seed(todoDbContext, seedingData);
+
+                // Assert
+                hasSeededSucceeded.Should().BeTrue();
+            }
+        }
+
+        /// <summary>
+        /// Tests <see cref="DatabaseSeeder.Seed"/> method.
+        /// </summary>
+        [Test]
+        public void Seed_WhenThereIsAlreadySomeData_MustReturnFalse()
+        {
+            // Arrange
+            var dbName = Guid.NewGuid().ToString();
+            var dbContextOptions = new DbContextOptionsBuilder<TodoDbContext>().UseInMemoryDatabase(dbName).Options;
+
+            using (var todoDbContext = new TodoDbContext(dbContextOptions))
+            {
+                todoDbContext.TodoItems.Add(new TodoItem());
+                todoDbContext.SaveChanges();
+
+                var loggerFactory = new NullLoggerFactory();
+                var logger = loggerFactory.CreateLogger<DatabaseSeeder>();
+                var databaseSeeder = new DatabaseSeeder(logger);
+                var seedingData = Enumerable.Range(1, 5).Select(index => new TodoItem { Id = index });
+
+                // Act 
+                var hasSeededSucceeded = databaseSeeder.Seed(todoDbContext, seedingData);
+
+                // Assert
+                hasSeededSucceeded.Should().BeFalse();
+            }
+        }
+    }
+}
---
+++ Tests/IntegrationTests/Todo.WebApi.IntegrationTests/Logging/LoggingMiddlewareTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace Todo.WebApi.Logging
+{
+    /// <summary>
+    ///  Contains integration tests targeting <see cref="LoggingMiddleware" /> class.
+    /// </summary>
+    [TestFixture]
+    public class LoggingMiddlewareTests
+    {
+    }
+}
