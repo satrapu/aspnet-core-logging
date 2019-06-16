135d05a9e9679ba4273536681c0f0814a8009063
Bogdan Marian <satrapu@users.noreply.github.com>
2019-06-16 22:02:57 +0300

    Install and start Docker using suggestion from GitHub
    See more here: https://github.com/Microsoft/azure-pipelines-image-generation/issues/738#issuecomment-496211237.

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -1,28 +1,12 @@
 #!/usr/bin/env bash
-# Based on this Stack Overflow answer: https://stackoverflow.com/a/35979292.
 
-case $1 in
-  -h|--help)
-    echo $'usage: docker-start\n\nStarts Docker (Docker.app) on macOS and waits until the Docker environment is initialized.'
-    exit 0
-    ;;
-esac
-(( $# )) && { echo "ARGUMENT ERROR: Unexpected argument(s) specified. Use -h for help." >&2; exit 2; }
+brew cask install docker
+sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
+/Applications/Docker.app/Contents/MacOS/Docker --unattended &
 
-[[ $(uname) == 'Darwin' ]] || { echo "This function only runs on macOS." >&2; exit 2; }
-
-echo "-- Starting Docker.app, if necessary..."
-open -g -a Docker.app || exit
-echo "-- Docker.app is running"
-
-# Wait for the server to start up, if applicable.  
-echo "-- Waiting for Docker server to start ..."
-
-i=0
-while ! docker system info &>/dev/null; do
-  (( i++ == 0 )) && printf %s '-- Waiting for Docker to finish starting up...' || printf '.'
-  sleep 1
+while ! docker info 2>/dev/null ; do
+  sleep 5
+  echo "Waiting for Docker service to be in the running state"
 done
-(( i )) && printf '\n'
 
-echo "-- Docker is ready."
+echo "Docker service is now running"
