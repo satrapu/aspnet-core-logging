74527dd2cb4362fe809cb1eaeac5bccd4297a5c2
Bogdan Marian <satrapu@users.noreply.github.com>
2019-11-03 22:23:45 +0200

    Apply community recommened fix when installing Docker on macOS

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # This script installs the latest version of Docker on macOS.
-# See more here: https://github.com/Microsoft/azure-pipelines-image-generation/issues/738.
-
-brew cask install docker
+# See more here: https://github.com/Microsoft/azure-pipelines-image-generation/issues/738
+# and here: https://github.com/microsoft/azure-pipelines-image-generation/issues/738#issuecomment-522301481.
+brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask/b8c67034bd78f9585b1316564f223b97055bc0dc/Casks/docker.rb
 sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
 /Applications/Docker.app/Contents/MacOS/Docker --unattended &
 
