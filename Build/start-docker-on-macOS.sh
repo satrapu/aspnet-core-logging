5b6e708c4c9fd400e561122b42c447455c854006
Bogdan Marian <satrapu@users.noreply.github.com>
2019-11-10 15:29:09 +0200

    Increase code readability by separating code blocks via new lines

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -2,19 +2,23 @@
 
 # Based on: https://github.com/microsoft/azure-pipelines-image-generation/issues/738#issuecomment-527013065.
 retries=0
+
 brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask/8ce4e89d10716666743b28c5a46cd54af59a9cc2/Casks/docker.rb
 sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
 /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+
 while ! docker info 2>/dev/null ; do
     sleep 5
-    retries=`expr $retries + 1`
+    retries=`expr ${retries} + 1`
+    
     if pgrep -xq -- "Docker"; then
         echo 'Docker still running'
     else
         echo 'Docker not running, restart'
         /Applications/Docker.app/Contents/MacOS/Docker --unattended &
     fi
-    if [ $retries -gt 30 ]; then
+    
+    if [[ ${retries} -gt 30 ]]; then
         >&2 echo 'Failed to run Docker'
         exit 1
     fi;
