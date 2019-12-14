5a452ddfa973e907b2069aa155b1a021368dd91e
Bogdan Marian <satrapu@users.noreply.github.com>
2019-12-14 21:49:04 +0200

    Fix referencing environment variable

--- Build/Provision-db4it-docker-container-using-healthcheck.ps1
+++ Build/Provision-db4it-docker-container-using-healthcheck.ps1
@@ -70,9 +70,9 @@ do {
         $dockerHostPort = docker port $ContainerName $dockerContainerPort
         $dockerHostPort = $dockerHostPort -split ':' | Select-Object -Skip 1
         $environmentVariableStoringDockerHostPort = $ContainerName + '_HostPort'
-        [Environment]::SetEnvironmentVariable($environmentVariableStoringDockerHostPort, $dockerHostPort, 'User')
-        $hostPort = [Environment]::GetEnvironmentVariable($environmentVariableStoringDockerHostPort, 'User')
-        Write-Output "Docker host port used by $ContainerName container is: $hostPort"
+        [Environment]::SetEnvironmentVariable($environmentVariableStoringDockerHostPort, "$dockerHostPort", 'User')
+        $dockerHostPort = [Environment]::GetEnvironmentVariable($environmentVariableStoringDockerHostPort, 'User')
+        Write-Output "Docker host port used by $ContainerName container is: $dockerHostPort"
         exit 0
     }
 
