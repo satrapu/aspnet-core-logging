af9a5d5eafe9e9c151c8f0b619bfdf488a9602b9
Bogdan Marian <satrapu@users.noreply.github.com>
2020-01-25 18:20:09 +0200

    Add more comments to make TodoItem entity class even more readable

--- Sources/Todo.Persistence/Entities/TodoItem.cs
+++ Sources/Todo.Persistence/Entities/TodoItem.cs
@@ -10,6 +10,7 @@ namespace Todo.Persistence.Entities
         /// <summary>
         /// Gets or sets the identifier of this action.
         /// </summary>
+        // ReSharper disable once UnassignedGetOnlyAutoProperty
         public long Id { get; }
 
         /// <summary>
@@ -19,17 +20,41 @@ namespace Todo.Persistence.Entities
 
         /// <summary>
         /// Gets or sets whether this action has been completed.
+        /// <br/>
+        /// When an item is created, by default this property is set to <code>false</code>.
         /// </summary>
         public bool IsComplete { get; set; }
 
+        /// <summary>
+        /// Gets the identifier of the user who has created this item.
+        /// </summary>
         public string CreatedBy { get; }
 
+        /// <summary>
+        /// Gets the date when this item was created.
+        /// </summary>
+        // ReSharper disable once MemberCanBePrivate.Global
         public DateTime CreatedOn { get; }
 
+        /// <summary>
+        /// Gets or sets the identifier of the user who last updated this item.
+        /// <br/>
+        /// When a user creates an item, this property is set to the same value as the <seealso cref="CreatedBy"/> one.
+        /// </summary>
         public string LastUpdatedBy { get; set; }
 
+        /// <summary>
+        /// Gets or sets the date when this item was last updated.
+        /// <br/>
+        /// When a user creates an item, this property is set to the same value as the <seealso cref="CreatedOn"/> one.
+        /// </summary>
         public DateTime? LastUpdatedOn { get; set; }
 
+        /// <summary>
+        /// Creates a new instance of the <see cref="TodoItem"/> class.
+        /// </summary>
+        /// <param name="name">The name ot this item.</param>
+        /// <param name="createdBy">The identifier of the user who's creating this item.</param>
         public TodoItem(string name, string createdBy)
         {
             Name = name;
@@ -41,7 +66,7 @@ namespace Todo.Persistence.Entities
         }
 
         /// <summary>
-        /// Gets the string representation of this action.
+        /// Gets the string representation of this item.
         /// </summary>
         /// <returns></returns>
         public override string ToString()
@@ -49,10 +74,10 @@ namespace Todo.Persistence.Entities
             return $"[{nameof(Id)}: {Id}, " +
                    $"{nameof(Name)}: {Name}, " +
                    $"{nameof(IsComplete)}: {IsComplete}, " +
-                   $"{nameof(CreatedBy)}: {CreatedBy}" +
-                   $"{nameof(CreatedOn)}: {CreatedOn:u}" +
-                   $"{nameof(LastUpdatedBy)}: {LastUpdatedBy}" +
-                   $"{nameof(LastUpdatedOn)}: {LastUpdatedOn:u}]";
+                   $"{nameof(CreatedBy)}: {CreatedBy}, " +
+                   $"{nameof(CreatedOn)}: {CreatedOn:u}, " +
+                   $"{nameof(LastUpdatedBy)}: {LastUpdatedBy}, " +
+                   $"{nameof(LastUpdatedOn)}: {LastUpdatedOn?.ToString("u")}]";
         }
     }
 }
