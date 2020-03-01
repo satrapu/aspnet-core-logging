d8e9fae38ae1f59355d636648e52b1c69eae0f16
Bogdan Marian <satrapu@users.noreply.github.com>
2020-03-01 23:00:19 +0200

    Fix attributes location

--- Sources/Todo.WebApi/Models/TodoItemQueryModel.cs
+++ Sources/Todo.WebApi/Models/TodoItemQueryModel.cs
@@ -10,9 +10,11 @@ namespace Todo.WebApi.Models
 
         public bool? IsComplete { get; set; }
 
-        [Range(1, 1000)] public int PageSize { get; set; } = 25;
+        [Range(1, 1000)] 
+        public int PageSize { get; set; } = 25;
 
-        [Range(0, int.MaxValue)] public int PageIndex { get; set; } = 0;
+        [Range(0, int.MaxValue)] 
+        public int PageIndex { get; set; } = 0;
 
         public string SortBy { get; set; }
 
