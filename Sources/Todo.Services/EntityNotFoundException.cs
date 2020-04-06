e1a1001c1bf3a13587d07272cf542dba006d295b
Bogdan Marian <satrapu@users.noreply.github.com>
2020-04-06 20:13:17 +0300

    Fix Sonar issue: "ISerializable" should be implemented correctly
    See more here: https://sonarcloud.io/project/issues?id=aspnet-core-logging&issues=AXEiBdTd5t5bp1Y1ECIB&open=AXEiBdTd5t5bp1Y1ECIB

--- Sources/Todo.Services/EntityNotFoundException.cs
+++ Sources/Todo.Services/EntityNotFoundException.cs
@@ -1,14 +1,30 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace Todo.Services
 {
     /// <summary>
     /// Thrown when failed to fetch an entity using a key.
     /// </summary>
+    [Serializable]
     public class EntityNotFoundException : Exception
     {
         public EntityNotFoundException(Type entityType, object entityKey) : base(
             $"Could not find entity of type \"{entityType?.FullName}\" using key \"{entityKey}\"")
+        {
+        }
+
+        /// <summary>
+        /// Serialization constructor.
+        /// </summary>
+        /// <param name="serializationInfo"></param>
+        /// <param name="streamingContext"></param>
+        protected EntityNotFoundException(SerializationInfo serializationInfo, StreamingContext streamingContext) :
+            base(serializationInfo, streamingContext)
+        {
+        }
+
+        public override void GetObjectData(SerializationInfo serializationInfo, StreamingContext streamingContext)
         {
         }
     }
