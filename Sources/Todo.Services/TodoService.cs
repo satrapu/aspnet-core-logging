8d75ebd595ca6f40b07eaca6df38e40bd94fe04e
Bogdan Marian <satrapu@users.noreply.github.com>
2020-05-14 22:55:28 +0300

    Tag EF Core query to make logs more readable

--- Sources/Todo.Services/TodoService.cs
+++ Sources/Todo.Services/TodoService.cs
@@ -42,7 +42,7 @@ namespace Todo.Services
         {
             Validator.ValidateObject(todoItemQuery, new ValidationContext(todoItemQuery), true);
 
-            IQueryable<TodoItem> todoItems = FilterItems(todoItemQuery);
+            IQueryable<TodoItem> todoItems = FilterItems(todoItemQuery).TagWith(nameof(GetByQueryAsync));
             todoItems = SortItems(todoItems, todoItemQuery);
             todoItems = PaginateItems(todoItems, todoItemQuery);
             IQueryable<TodoItemInfo> todoItemInfos = ProjectItems(todoItems);
@@ -64,7 +64,7 @@ namespace Todo.Services
                 IsComplete = newTodoItemInfo.IsComplete.Value
             };
 
-            todoDbContext.TodoItems.Add(newTodoItem);
+            await todoDbContext.TodoItems.AddAsync(newTodoItem);
             await todoDbContext.SaveChangesAsync().ConfigureAwait(false);
 
             logger.LogInformation("Item with id {TodoItemId} has been added by user {UserId}"
