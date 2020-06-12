aeb3476e2cd37356ae6d788f0dcb5f6a9e0224db
Bogdan Marian <satrapu@users.noreply.github.com>
2020-06-12 19:50:29 +0300

    Remove logging connection string as CI pipeline has been fixed

--- Tests/IntegrationTests/Todo.WebApi.IntegrationTests/Infrastructure/TestWebApplicationFactory.cs
+++ Tests/IntegrationTests/Todo.WebApi.IntegrationTests/Infrastructure/TestWebApplicationFactory.cs
@@ -84,7 +84,6 @@ namespace Todo.WebApi.Infrastructure
 
         private void SetupDbContext(IServiceCollection services)
         {
-            ILogger logger = services.BuildServiceProvider().GetRequiredService<ILogger<TestWebApplicationFactory>>();
             ServiceDescriptor dbContextServiceDescriptor = services.SingleOrDefault(serviceDescriptor =>
                 serviceDescriptor.ServiceType == typeof(DbContextOptions<TodoDbContext>));
 
@@ -102,9 +101,6 @@ namespace Todo.WebApi.Infrastructure
                 {
                     Database = testDatabaseName
                 };
-
-                logger.LogInformation("Integration tests will use the following connection string: {ConnectionString}",
-                    connectionStringBuilder.ConnectionString);
 
                 dbContextOptionsBuilder.UseNpgsql(connectionStringBuilder.ConnectionString)
                     .EnableSensitiveDataLogging()
