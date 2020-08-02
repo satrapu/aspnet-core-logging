74cb8a968006b82db67e47bc4bb13c93f515d23f
Bogdan Marian <satrapu@users.noreply.github.com>
2020-08-02 21:23:13 +0300

    Install Docker Compose via brew on macOS agent

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Based on: https://github.com/microsoft/azure-pipelines-image-generation/issues/738#issuecomment-527013065.
-retries=0
+echo "About to install Docker ..."
 
 # Install working Docker version 2.0.0.3-ce-mac81,31259
 brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask/8ce4e89d10716666743b28c5a46cd54af59a9cc2/Casks/docker.rb
 sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
 /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+echo "Docker has been installed"
+
+# Waiting for Docker service to start
+echo "Waiting for Docker service to be in the running state ..."
+retries=0
 
 while ! docker info 2>/dev/null ; do
     sleep 5
@@ -28,3 +33,8 @@ while ! docker info 2>/dev/null ; do
 done
 
 echo "Docker service is now running"
+
+# Install Docker Compose
+echo "About to install Docker Compose ..."
+brew install docker-compose
+echo "Docker Compose has been installed"
