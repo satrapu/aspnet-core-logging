4d3627bbba24e92ddc6bf2e929706ab32cda241c
Bogdan Marian <satrapu@users.noreply.github.com>
2020-11-02 22:25:42 +0200

    Add more comments to the application business flows

--- Sources/Todo.WebApi/Logging/BusinessFlowNames.cs
+++ Sources/Todo.WebApi/Logging/BusinessFlowNames.cs
@@ -6,13 +6,17 @@
     public static class BusinessFlowNames
     {
         public const string ScopeKey = "BusinessFlowName";
-        
+
         /// <summary>
-        /// Contains the business flow names related to authentication.
+        /// Contains the business flow names related to authentication & authorization.
         /// </summary>
-        public static class Authentication
+        public static class Security
         {
-            private const string Prefix = nameof(Authentication);
+            private const string Prefix = nameof(Security);
+
+            /// <summary>
+            /// Business flow used for generating a JSON web token to be used for authorization purposes.
+            /// </summary>
             public static readonly string GenerateJwtToken = $"{Prefix}/{nameof(GenerateJwtToken)}";
         }
 
@@ -23,14 +27,29 @@
         {
             private const string Prefix = nameof(Crud);
 
+            /// <summary>
+            /// Business flow used for creating a new todo item.
+            /// </summary>
             public static readonly string CreateTodoItem = $"{Prefix}/{nameof(CreateTodoItem)}";
-            
+
+            /// <summary>
+            /// Business flow used for updating an existing todo item.
+            /// </summary>
             public static readonly string UpdateTodoItem = $"{Prefix}/{nameof(UpdateTodoItem)}";
-            
+
+            /// <summary>
+            /// Business flow used for removing an existing todo item.
+            /// </summary>
             public static readonly string DeleteTodoItem = $"{Prefix}/{nameof(DeleteTodoItem)}";
-            
+
+            /// <summary>
+            /// Business flow used for fetching the details of an existing todo item.
+            /// </summary>
             public static readonly string GetTodoItem = $"{Prefix}/{nameof(GetTodoItem)}";
-            
+
+            /// <summary>
+            /// Business flow used for fetching the details of a list of existing todo items.
+            /// </summary>
             public static readonly string GetTodoItems = $"{Prefix}/{nameof(GetTodoItems)}";
         }
     }
