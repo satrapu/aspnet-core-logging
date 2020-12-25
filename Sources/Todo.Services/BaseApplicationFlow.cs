f50f7057ac1e8343df1f78beae281cf6336ec933
Bogdan Marian <satrapu@users.noreply.github.com>
2020-12-25 15:36:00 +0200

    Add base class for application flows

---
+++ Sources/Todo.Services/BaseApplicationFlow.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Transactions;
+using Microsoft.Extensions.Logging;
+
+namespace Todo.Services
+{
+    /// <summary>
+    /// Base class for all application flows.
+    /// </summary>
+    public abstract class BaseApplicationFlow<TInput, TOutput> : IApplicationFlow<TInput, TOutput>
+    {
+        private readonly string flowName;
+        private readonly ILogger logger;
+
+        protected BaseApplicationFlow(string flowName, ILogger logger)
+        {
+            if (string.IsNullOrWhiteSpace(flowName))
+            {
+                throw new ArgumentException(nameof(flowName));
+            }
+
+            this.flowName = flowName;
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<TOutput> ExecuteAsync(TInput input, IPrincipal flowInitiator)
+        {
+            using (logger.BeginScope(new Dictionary<string, object> {["BusinessFlowName"] = flowName}))
+            {
+                bool isSuccess = false;
+                Stopwatch stopwatch = Stopwatch.StartNew();
+                string flowInitiatorName = flowInitiator.GetNameOrDefault();
+
+                try
+                {
+                    logger.LogInformation("User [{User}] has started application flow [{ApplicationFlow}] ...",
+                        flowInitiatorName, flowName);
+                    TOutput output = await ExecuteFlowAsync(input).ConfigureAwait(false);
+                    isSuccess = true;
+                    return output;
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                    logger.LogInformation("User [{User}] has finished application flow [{ApplicationFlow}] "
+                                          + "with the outcome: [{ApplicationFlowOutcome}]; time taken: [{ApplicationFlowDuration}]",
+                        flowInitiatorName, flowName, isSuccess ? "success" : "failure",
+                        stopwatch.Elapsed);
+                }
+            }
+        }
+
+        private async Task<TOutput> ExecuteFlowAsync(TInput input)
+        {
+            Validator.ValidateObject(input, new ValidationContext(input), validateAllProperties: true);
+
+            var transactionOptions = new TransactionOptions
+            {
+                IsolationLevel = IsolationLevel.ReadCommitted,
+                Timeout = TimeSpan.FromSeconds(value: 60)
+            };
+
+            using var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions,
+                TransactionScopeAsyncFlowOption.Enabled);
+            TOutput output = await ExecuteFlowStepsAsync(input).ConfigureAwait(false);
+            transactionScope.Complete();
+            return output;
+        }
+
+        protected abstract Task<TOutput> ExecuteFlowStepsAsync(TInput input);
+    }
+}
---
+++ Sources/Todo.Services/IApplicationFlow.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace Todo.Services
+{
+    /// <summary>
+    /// An application flow represents the implementation of one specific full blown feature requested by the business.
+    /// </summary>
+    /// <typeparam name="TInput">The type of the input needed to execute this flow.</typeparam>
+    /// <typeparam name="TOutput">The type of the outcome of this flow.</typeparam>
+    public interface IApplicationFlow<in TInput, TOutput>
+    {
+        /// <summary>
+        /// Executes this flow.
+        /// </summary>
+        /// <param name="input">The input needed to execute this flow.</param>
+        /// <param name="flowInitiator">The user who initiated executing this flow.</param>
+        /// <returns>An instance of <see cref="TOutput"/> type.</returns>
+        Task<TOutput> ExecuteAsync(TInput input, IPrincipal flowInitiator);
+    }
+}
