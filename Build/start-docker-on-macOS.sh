de4e6a5eef5fac88ee9a27f0e9b14d32fee091ba
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-03 22:55:43 +0200

    Install latest version of Docker on macOS-based Azure DevOps agents

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -10,31 +10,4 @@ set -o nounset
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577574.
 set -o errexit
 
-# Install working Docker version 2.0.0.3-ce-mac81,31259
-brew install --cask https://raw.githubusercontent.com/Homebrew/homebrew-cask/8ce4e89d10716666743b28c5a46cd54af59a9cc2/Casks/docker.rb
-sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
-/Applications/Docker.app/Contents/MacOS/Docker --unattended &
-
-retries=0
-maxRetries=30
-
-while ! docker info 2>/dev/null ; do
-    sleep 5s
-    ((retries=retries+1))
-    
-    if pgrep -xq -- 'Docker'; then
-        echo 'Docker service still running'
-    else
-        echo 'Docker service not running, restart'
-        /Applications/Docker.app/Contents/MacOS/Docker --unattended &
-    fi
-    
-    if [[ ${retries} -gt ${maxRetries} ]]; then
-        >&2 echo 'Failed to run Docker service'
-        exit 1
-    fi;
-
-    echo 'Waiting for Docker service to be in the running state'
-done
-
-echo 'Docker service is now running'
+brew install docker
