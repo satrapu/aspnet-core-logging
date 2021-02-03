09c88d2f72b463f4ee43dfe189829a004350378a
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-03 23:01:37 +0200

    Use 'cask' when installing Docker via brew

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -10,4 +10,14 @@ set -o nounset
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577574.
 set -o errexit
 
-brew install docker
+echo 'Installing Docker ...'
+brew cask install docker
+echo 'Docker has been installed'
+
+echo 'Checking Docker version ...'
+docker --version
+
+echo 'Checking Docker Compose version ...'
+docker-compose --version
+
+echo 'All good :)'
