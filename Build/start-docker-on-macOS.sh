65ce42370f2ad942b7a2cd39d28b4c40b5ca7007
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-03 23:29:17 +0200

    Ensure Docker service has started before running any 'docker' commands

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -10,13 +10,38 @@ set -o nounset
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577574.
 set -o errexit
 
+# Install latest version of Docker Desktop for Mac
 echo 'Installing Docker Desktop for Mac ...'
 brew install --cask docker
 echo 'Docker Desktop for Mac has been installed'
 
-echo 'Starting Docker Desktop for Mac'
-open /Applications/Docker.app
-echo 'Docker Desktop for Mac has started'
+echo 'Starting Docker service ...'
+sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
+/Applications/Docker.app/Contents/MacOS/Docker --unattended &
+
+retries=0
+maxRetries=30
+
+while ! docker info 2>/dev/null ; do
+    sleep 5s
+    ((retries=retries+1))
+    
+    if pgrep -xq -- 'Docker'; then
+        echo 'Docker service still running'
+    else
+        echo 'Docker service not running, restart'
+        /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+    fi
+    
+    if [[ ${retries} -gt ${maxRetries} ]]; then
+        >&2 echo 'Failed to run Docker service'
+        exit 1
+    fi;
+
+    echo 'Waiting for Docker service to be in the running state'
+done
+
+echo 'Docker service is now running'
 
 echo 'Checking Docker version ...'
 docker --version
