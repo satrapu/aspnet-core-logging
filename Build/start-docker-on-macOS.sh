762c74de9518c07d9f38cca4fccb5aae592181cd
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-05 23:23:28 +0200

    Ensure 'brew' command output is not displayed

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -12,7 +12,7 @@ set -o errexit
 
 # Install latest version of Docker Desktop for Mac
 echo 'Installing Docker Desktop for Mac ...'
-brew install --cask docker
+brew install --cask docker &>/dev/null
 
 # Allow Docker.app to run without confirmation
 xattr -d -r com.apple.quarantine /Applications/Docker.app
@@ -25,22 +25,26 @@ sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
 sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
 echo 'Docker Desktop for Mac has been installed'
 
-echo "Starting Docker.app, if necessary..."
+printf '\n\n'
+echo 'Starting Docker Desktop for Mac, if necessary ...'
 open -g -a Docker.app || exit
 
 # Wait for the server to start up, if applicable.
 i=0
 while ! docker system info &>/dev/null; do
-  (( i++ == 0 )) && printf %s 'Waiting for Docker to finish starting up...' || printf '.'
-  sleep 1
+  (( i++ == 0 )) && printf %s 'Waiting for Docker Desktop for Mac to finish starting up ...' || printf '.'
+  sleep 5
 done
 (( i )) && printf '\n'
-echo "Docker is ready."
+echo 'Docker Desktop for Mac is ready to be used'
 
+printf '\n\n'
 echo 'Displaying Docker version ...'
 docker --version
 
+printf '\n\n'
 echo 'Displaying Docker Compose version ...'
 docker-compose --version
 
+printf '\n\n'
 echo 'All good :)'
