4615706e4d0f7ac30553bf2c28e64deb5a13db0c
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-05 22:49:38 +0200

    Use a different approach when installing Docker Desktop for Mac
    Based on telamonian's comment on a GitHub issue: https://github.com/docker/for-mac/issues/2359#issuecomment-607154849.

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Based on: https://github.com/microsoft/azure-pipelines-image-generation/issues/738#issuecomment-527013065.
+# Based on: https://github.com/docker/for-mac/issues/2359#issuecomment-607154849.
 
 # Fail script in case of unset variables - see more here: 
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577541.
@@ -12,13 +12,35 @@ set -o errexit
 
 # Install latest version of Docker Desktop for Mac
 echo 'Installing Docker Desktop for Mac ...'
-brew install --cask docker
+brew cask install docker
+
+# Allow Docker.app to run without confirmation
+xattr -d -r com.apple.quarantine /Applications/Docker.app
+
+# Preemptively do Docker.app's setup to avoid any GUI prompts
+sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
+sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
+sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
+sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
+sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
 echo 'Docker Desktop for Mac has been installed'
 
-echo 'Checking Docker version ...'
+echo "Starting Docker.app, if necessary..."
+open -g -a Docker.app || exit
+
+# Wait for the server to start up, if applicable.
+i=0
+while ! docker system info &>/dev/null; do
+  (( i++ == 0 )) && printf %s 'Waiting for Docker to finish starting up...' || printf '.'
+  sleep 1
+done
+(( i )) && printf '\n'
+echo "Docker is ready."
+
+echo 'Displaying Docker version ...'
 docker --version
 
-echo 'Checking Docker Compose version ...'
+echo 'Displaying Docker Compose version ...'
 docker-compose --version
 
 echo 'All good :)'
