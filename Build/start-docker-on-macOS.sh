ab6df43eb24104a018598bf0bf3923a78bf3ee51
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-05 22:15:36 +0200

    Tweak a little the command used for installing Docker Desktop for Mac

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -12,36 +12,8 @@ set -o errexit
 
 # Install latest version of Docker Desktop for Mac
 echo 'Installing Docker Desktop for Mac ...'
-brew install --cask docker
+brew cask install docker
 echo 'Docker Desktop for Mac has been installed'
-
-echo 'Starting Docker service ...'
-/Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
-/Applications/Docker.app/Contents/MacOS/Docker --unattended &
-
-retries=0
-maxRetries=30
-
-while ! docker info 2>/dev/null ; do
-    sleep 5s
-    ((retries=retries+1))
-    
-    if pgrep -xq -- 'Docker'; then
-        echo 'Docker service still running'
-    else
-        echo 'Docker service not running, restart'
-        /Applications/Docker.app/Contents/MacOS/Docker --unattended &
-    fi
-    
-    if [[ ${retries} -gt ${maxRetries} ]]; then
-        >&2 echo 'Failed to run Docker service'
-        exit 1
-    fi;
-
-    echo 'Waiting for Docker service to be in the running state'
-done
-
-echo 'Docker service is now running'
 
 echo 'Checking Docker version ...'
 docker --version
