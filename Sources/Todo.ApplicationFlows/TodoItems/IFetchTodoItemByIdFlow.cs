8431a8f5dc0be49746f4fd9b906e019f1996fdec
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-13 18:32:16 +0200

    Remove no longer needed TODO

--- Sources/Todo.ApplicationFlows/TodoItems/IFetchTodoItemByIdFlow.cs
+++ Sources/Todo.ApplicationFlows/TodoItems/IFetchTodoItemByIdFlow.cs
@@ -7,7 +7,5 @@ namespace Todo.ApplicationFlows.TodoItems
     /// </summary>
     public interface IFetchTodoItemByIdFlow : IApplicationFlow<long, TodoItemInfo>
     {
-        // TODO: Bogdan Marian 2021-01-01: Use a model decorated with validation related attributes instead
-        // of a raw long value.
     }
 }
