07d07519590a5e09f42fa5c606def5165f7c3759
Bogdan Marian <satrapu@users.noreply.github.com>
2021-02-21 20:35:47 +0200

    Ensure trying to modify same entity from 2 concurrent transactions fails

--- Tests/IntegrationTests/Todo.Persistence.IntegrationTests/TodoDbContextTests.cs
+++ Tests/IntegrationTests/Todo.Persistence.IntegrationTests/TodoDbContextTests.cs
@@ -1,12 +1,15 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Threading.Tasks;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Npgsql;
 using NUnit.Framework;
+using Todo.Persistence.Entities;
 
 namespace Todo.Persistence
 {
@@ -22,37 +25,110 @@ namespace Todo.Persistence
         /// Checks whether EF Core migrations can be run in both directions, up and down.
         /// </summary>
         [Test]
-        public void GivenTheCurrentMigrations_WhenApplyingAndRevertingAndApplyingThemAgain_DatabaseIsCorrectlyMigrated()
+        public async Task GivenTheCurrentMigrations_WhenRunningThemInBothDirections_DatabaseIsCorrectlyMigrated()
         {
             // Arrange
-            DbContextOptions<TodoDbContext> dbContextOptions = GetDbContextOptions();
-
-            using TodoDbContext todoDbContext = new TodoDbContext(dbContextOptions);
+            string databaseName =
+                $"it--{nameof(GivenTheCurrentMigrations_WhenRunningThemInBothDirections_DatabaseIsCorrectlyMigrated)}";
+            DbContextOptions<TodoDbContext> dbContextOptions = GetDbContextOptions(databaseName);
+            await using TodoDbContext todoDbContext = new TodoDbContext(dbContextOptions);
             bool isMigrationSuccessful;
 
             try
             {
-                todoDbContext.Database.EnsureDeleted();
+                await todoDbContext.Database.EnsureDeletedAsync();
                 IMigrator databaseMigrator = todoDbContext.GetInfrastructure().GetRequiredService<IMigrator>();
 
                 // Act
-                databaseMigrator.Migrate();
+                await databaseMigrator.MigrateAsync();
                 // Revert migrations by using a special migration identifier.
                 // See more here: https://docs.microsoft.com/en-us/ef/core/miscellaneous/cli/dotnet#dotnet-ef-database-update.
-                databaseMigrator.Migrate(BeforeFirstDatabaseMigration);
-                databaseMigrator.Migrate();
+                await databaseMigrator.MigrateAsync(BeforeFirstDatabaseMigration);
+                await databaseMigrator.MigrateAsync();
                 isMigrationSuccessful = true;
             }
             catch
             {
                 isMigrationSuccessful = false;
             }
+            finally
+            {
+                await todoDbContext.Database.EnsureDeletedAsync();
+            }
 
             // Assert
             isMigrationSuccessful.Should().BeTrue("migrations should work in both directions, up and down");
         }
 
-        private static DbContextOptions<TodoDbContext> GetDbContextOptions()
+        /// <summary>
+        /// Ensures that trying to modify same entity from withing 2 concurrent transactions fails.
+        /// </summary>
+        /// <returns></returns>
+        [Test]
+        public async Task SaveChanges_WhenModifyingSameEntityUsingTwoConcurrentTransactions_ThrowsException()
+        {
+            // Arrange
+            string databaseName =
+                $"it--{nameof(SaveChanges_WhenModifyingSameEntityUsingTwoConcurrentTransactions_ThrowsException)}";
+            DbContextOptions<TodoDbContext> dbContextOptions = GetDbContextOptions(databaseName);
+
+            await using TodoDbContext firstTodoDbContext = new TodoDbContext(dbContextOptions);
+            IMigrator databaseMigrator = firstTodoDbContext.GetInfrastructure().GetRequiredService<IMigrator>();
+            await databaseMigrator.MigrateAsync();
+
+            try
+            {
+                string name = "ConcurrentlyAccessedTodoItem";
+
+                TodoItem todoItem = new TodoItem(name, "it")
+                {
+                    IsComplete = false,
+                };
+
+                await firstTodoDbContext.TodoItems.AddAsync(todoItem);
+                await firstTodoDbContext.SaveChangesAsync();
+
+                await using IDbContextTransaction firstTransaction =
+                    await firstTodoDbContext.Database.BeginTransactionAsync();
+                TodoItem todoItemFromFirstTransaction =
+                    await firstTodoDbContext.TodoItems.FirstAsync(x => x.Name == name);
+                todoItemFromFirstTransaction.IsComplete = true;
+                todoItemFromFirstTransaction.LastUpdatedBy = Guid.NewGuid().ToString("N");
+                todoItemFromFirstTransaction.LastUpdatedOn = DateTime.UtcNow;
+
+                await using TodoDbContext secondTodoDbContext = new TodoDbContext(dbContextOptions);
+                await using IDbContextTransaction secondTransaction =
+                    await secondTodoDbContext.Database.BeginTransactionAsync();
+                TodoItem todoItemFromSecondTransaction =
+                    await secondTodoDbContext.TodoItems.FirstAsync(x => x.Name == name);
+                todoItemFromSecondTransaction.IsComplete = false;
+                todoItemFromSecondTransaction.LastUpdatedBy = Guid.NewGuid().ToString("N");
+                todoItemFromSecondTransaction.LastUpdatedOn = DateTime.UtcNow;
+
+                await firstTodoDbContext.SaveChangesAsync();
+                await firstTransaction.CommitAsync();
+                                
+                // Act
+                Func<Task> saveChangesAsyncCall = async () =>
+                {
+                    // ReSharper disable AccessToDisposedClosure
+                    await secondTodoDbContext.SaveChangesAsync();
+                    await secondTransaction.CommitAsync();
+                    // ReSharper restore AccessToDisposedClosure
+                };
+
+                // Assert
+                saveChangesAsyncCall.Should()
+                    .ThrowExactly<DbUpdateConcurrencyException>(
+                        "2 transactions were concurrently modifying the same entity");
+            }
+            finally
+            {
+                await firstTodoDbContext.Database.EnsureDeletedAsync();
+            }
+        }
+
+        private static DbContextOptions<TodoDbContext> GetDbContextOptions(string databaseName)
         {
             var configurationBuilder = new ConfigurationBuilder();
             IConfigurationRoot testConfiguration = configurationBuilder.AddJsonFile("appsettings.json", false)
@@ -63,7 +139,7 @@ namespace Todo.Persistence
             var testConnectionString = testConfiguration.GetConnectionString("TodoForIntegrationTests");
             var connectionStringBuilder = new NpgsqlConnectionStringBuilder(testConnectionString)
             {
-                Database = $"it--{MethodBase.GetCurrentMethod()?.DeclaringType?.Name}"
+                Database = $"it--{databaseName}"
             };
 
             DbContextOptions<TodoDbContext> dbContextOptions = new DbContextOptionsBuilder<TodoDbContext>()
