1b6d28d0b3df6acad82681a3088fec35abd42c07
Bogdan Marian <satrapu@users.noreply.github.com>
2021-04-04 19:15:03 +0300

    Explicitly specify env var file when starting compose services

--- Build/RunComposeServices.ps1
+++ Build/RunComposeServices.ps1
@@ -92,6 +92,7 @@ Write-Output $InfoMessage
 # standard error stream, thus tricking runtime into thinking it has failed.
 docker-compose --file="$ComposeFilePath" `
                --project-name="$ComposeProjectName" `
+               --env-file="$RelativePathToEnvironmentFile" `
                up `
                --detach
 
