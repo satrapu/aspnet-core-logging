223e4ecfd424d3b7a25da9cb6c02fc3ab7a36722
Bogdan Marian <satrapu@users.noreply.github.com>
2021-04-16 22:34:55 +0300

    Fix path to Docker.app folder on macOS-based agents

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -31,9 +31,14 @@ duration=$(( end - start ))
 echo "Docker Desktop for Mac has been installed in $duration seconds"
 
 printf '\n\n'
+
+echo 'Locating Docker.app folder ...'
+find / -type d -name 'Docker.app'
+printf '\n\n'
+
 echo 'Starting Docker Desktop for Mac, if necessary ...'
 start=$SECONDS
-open -g -a Docker.app || exit
+open -g -a /Applications/Docker.app || exit
 
 # Wait for the server to start up, if applicable.
 i=0
