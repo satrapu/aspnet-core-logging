2a5ae38fdcf079152adffecfc2818edf892f32d9
Bogdan Marian <satrapu@users.noreply.github.com>
2021-04-20 22:04:27 +0300

    Fix issues reported by dotnet-format tool

--- Sources/Todo.WebApi/Logging/ApplicationInsightsOptions.cs
+++ Sources/Todo.WebApi/Logging/ApplicationInsightsOptions.cs
@@ -1,4 +1,4 @@
-﻿namespace Todo.WebApi.Logging
+namespace Todo.WebApi.Logging
 {
     /// <summary>
     /// Stores Azure Application Insights configuration.
@@ -8,6 +8,6 @@
         /// <summary>
         /// Gets or sets the instrumentation key pointing to the proper Azure Application Insights instance.
         /// </summary>
-        public string InstrumentationKey {get; set;}
+        public string InstrumentationKey { get; set; }
     }
 }
