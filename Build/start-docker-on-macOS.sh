1f88b395b4946e8e3476ab0b1c31efb3491fc039
Bogdan Marian <satrapu@users.noreply.github.com>
2021-05-28 21:21:42 +0300

    Install older Docker version on macOS
    This older Docker version used to work in
    September 2020 and since the current way of
    installing Docker Desktop on macOS no longer
    works on macOS-based agents, I need to
    resort to installing an older version of Docker,
    but one which actually works (hopefully!).

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -1,59 +1,40 @@
 #!/bin/bash
 
-# Based on: https://github.com/docker/for-mac/issues/2359#issuecomment-607154849.
+# Based on: https://github.com/microsoft/azure-pipelines-image-generation/issues/738#issuecomment-527013065.
 
-# Fail script in case of unset variables - see more here: 
+# Fail script in case of unset variables - see more here:
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577541.
 set -o nounset
- 
+
 # Fail scripts in case a command fails - see more here:
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577574.
 set -o errexit
 
-# Install latest version of Docker Desktop for Mac
-echo 'Installing Docker Desktop for Mac ...'
-start=$SECONDS
+# Install working Docker version 2.0.0.3-ce-mac81,31259
+brew cask install https://raw.githubusercontent.com/Homebrew/homebrew-cask/8ce4e89d10716666743b28c5a46cd54af59a9cc2/Casks/docker.rb
+sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
+/Applications/Docker.app/Contents/MacOS/Docker --unattended &
 
-brew install --cask docker &>/dev/null
+retries=0
+maxRetries=30
 
-# Allow Docker.app to run without confirmation
-xattr -d -r com.apple.quarantine /Applications/Docker.app
+while ! docker info 2>/dev/null ; do
+    sleep 5s
+    ((retries=retries+1))
 
-# Preemptively do Docker.app's setup to avoid any GUI prompts
-sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
-sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
-sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
-sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
-sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
+    if pgrep -xq -- "Docker"; then
+        echo 'Docker service still running'
+    else
+        echo 'Docker service not running, restart'
+        /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+    fi
 
-end=$SECONDS
-duration=$(( end - start ))
-echo "Docker Desktop for Mac has been installed in $duration seconds"
+    if [[ ${retries} -gt ${maxRetries} ]]; then
+        >&2 echo 'Failed to run Docker service'
+        exit 1
+    fi;
 
-printf '\n\n'
-echo 'Starting Docker Desktop for Mac, if necessary ...'
-start=$SECONDS
-open -g -a /Applications/Docker.app || exit
-
-# Wait for the server to start up, if applicable.
-i=0
-while ! docker system info &>/dev/null; do
-  (( i++ == 0 )) && printf %s 'Waiting for Docker Desktop for Mac to finish starting up ...' || printf '.'
-  sleep 5
+    echo 'Waiting for Docker service to be in the running state'
 done
-(( i )) && printf '\n'
 
-end=$SECONDS
-duration=$(( end - start ))
-echo "Docker Desktop for Mac is ready to be used (after $duration seconds)"
-
-printf '\n\n'
-echo 'Displaying Docker version ...'
-docker --version
-
-printf '\n\n'
-echo 'Displaying Docker Compose version ...'
-docker-compose --version
-
-printf '\n\n'
-echo 'All good :)'
+echo "Docker service is now running"
