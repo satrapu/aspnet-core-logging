52db9479b3f2939e51875ce65657824d60cc296b
Bogdan Marian <satrapu@users.noreply.github.com>
2021-05-29 21:54:48 +0300

    Fix installing Docker Desktop for Mac inside macOS-based Azure DevOps agents (#18)
    * Install specific Docker version in macOS build agents
    
    * Fix brew install command
    
    * Fix brew install command, take 2
    
    * Fix brew install command, take 3
    
    * Fix brew install command, take 4
    
    * Fix brew install command, take 5
    
    * Fix brew install command, take 6
    
    * Fix brew install command, take 7
    
    * Fix brew install command, take 8
    
    * Enable build on macOS-based agents
    
    * Install older Docker version on macOS
    
    This older Docker version used to work in
    September 2020 and since the current way of
    installing Docker Desktop on macOS no longer
    works on macOS-based agents, I need to
    resort to installing an older version of Docker,
    but one which actually works (hopefully!).
    
    * Fix 'brew install' command syntax
    
    See more about this fix here: https://stackoverflow
    .com/a/66081492/5786708.
    
    * Download Docker installation script using curl
    
    * Print Docker and Docker Compose versions
    
    * Fix relative paths pointing to compose files
    
    * Rename folder to match the solution one
    
    * Rename folder to match the solution one
    
    * Remove redundant compose .env file
    
    Since this file is used for starting
    a compose workload inside a CI
    pipeline, all key/value pairs found
    in this file can be safely moved to
    the docker-compose.yml file, thus
    the compose maintainer needs to look
    into one file only.
    Another reason for removing this file
    is that macOS-based agents use an
    older version of Docker Compose
    which seems it cannot handle the
    `--env-file` CLI parameter.
    
    * Remove no longer needed file
    
    * Specify versions for Docker and Docker Compose
    
    * Install latest version of Docker brew formula
    
    * Install Docker Desktop for Mac, v3.3.0,62916.
    
    * Install Docker Desktop for Mac, v3.2.0,61504
    
    * Install Docker Desktop for Mac, v3.1.0,51484
    
    * Install Docker Desktop for Mac, v3.0.0,50684
    
    * Install Docker Desktop for Mac, v2.0.0.3,31259

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -1,59 +1,66 @@
 #!/bin/bash
 
-# Based on: https://github.com/docker/for-mac/issues/2359#issuecomment-607154849.
+# Based on: http://web.archive.org/web/20201012054023if_/https://github.com/microsoft/azure-pipelines-image-generation/issues/738#issuecomment-522301481.
+# The original GitHub issue link is no longer available, thus I had to resort to the Wayback Machine URL!
 
-# Fail script in case of unset variables - see more here: 
+# Fail script in case of unset variables - see more here:
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577541.
 set -o nounset
- 
+
 # Fail scripts in case a command fails - see more here:
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577574.
 set -o errexit
 
 # Install latest version of Docker Desktop for Mac
-echo 'Installing Docker Desktop for Mac ...'
+echo 'Downloading and then running docker brew formula ...'
 start=$SECONDS
 
-brew install --cask docker &>/dev/null
+# The brew formula below will install Docker Desktop for Mac, v2.0.0.3,31259.
+dockerInstallationScriptName='docker.rb'
+dockerInstallationScriptUrl="https://raw.githubusercontent.com/Homebrew/homebrew-cask/8ce4e89d10716666743b28c5a46cd54af59a9cc2/Casks/$dockerInstallationScriptName"
+curl -L  $dockerInstallationScriptUrl > $dockerInstallationScriptName && brew install $dockerInstallationScriptName
 
-# Allow Docker.app to run without confirmation
-xattr -d -r com.apple.quarantine /Applications/Docker.app
+end=$SECONDS
+duration=$(( end - start ))
+echo "Docker brew formula has been downloaded & run in $duration seconds"
 
-# Preemptively do Docker.app's setup to avoid any GUI prompts
-sudo /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools
-sudo /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
-sudo /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
-sudo /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
-sudo /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
+echo 'Installing Docker Desktop for Mac ...'
+start=$SECONDS
+sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
+/Applications/Docker.app/Contents/MacOS/Docker --unattended &
 
 end=$SECONDS
 duration=$(( end - start ))
 echo "Docker Desktop for Mac has been installed in $duration seconds"
 
-printf '\n\n'
-echo 'Starting Docker Desktop for Mac, if necessary ...'
+echo 'Starting Docker service ...'
 start=$SECONDS
-open -g -a /Applications/Docker.app || exit
 
-# Wait for the server to start up, if applicable.
-i=0
-while ! docker system info &>/dev/null; do
-  (( i++ == 0 )) && printf %s 'Waiting for Docker Desktop for Mac to finish starting up ...' || printf '.'
-  sleep 5
+retries=0
+maxRetries=30
+
+while ! docker info 2>/dev/null ; do
+    sleep 5s
+    ((retries=retries+1))
+
+    if pgrep -xq -- 'Docker'; then
+        echo 'Docker service is still booting'
+    else
+        echo 'Docker service is no longer running, need to restart it'
+        /Applications/Docker.app/Contents/MacOS/Docker --unattended &
+    fi
+
+    if [[ ${retries} -gt ${maxRetries} ]]; then
+        >&2 echo 'Docker service failed to enter running state during the expected time'
+        exit 1
+    fi;
+
+    echo 'Waiting for Docker service to enter running state ...'
 done
-(( i )) && printf '\n'
 
 end=$SECONDS
 duration=$(( end - start ))
-echo "Docker Desktop for Mac is ready to be used (after $duration seconds)"
+echo "Docker service has started after $duration seconds"
 
-printf '\n\n'
-echo 'Displaying Docker version ...'
 docker --version
-
-printf '\n\n'
-echo 'Displaying Docker Compose version ...'
 docker-compose --version
-
-printf '\n\n'
-echo 'All good :)'
