b1554970b72c3beed36dc82068d5f2c7512138f9
Bogdan Marian <satrapu@users.noreply.github.com>
2021-05-31 16:25:18 +0300

    Disable DB migrations for this test class

--- Tests/IntegrationTests/Todo.WebApi.IntegrationTests/Controllers/ConfigurationControllerTests.cs
+++ Tests/IntegrationTests/Todo.WebApi.IntegrationTests/Controllers/ConfigurationControllerTests.cs
@@ -36,7 +36,11 @@ namespace Todo.WebApi.Controllers
                         configurationBuilder.AddInMemoryCollection(new[]
                         {
                             new KeyValuePair<string, string>("ConfigurationDebugViewEndpointEnabled",
-                                isDebugViewEnabled.ToString())
+                                isDebugViewEnabled.ToString()),
+
+                            // Ensure database is not migrated while running tests found in this test class, no matter
+                            // the environment specified in this method.
+                            new KeyValuePair<string, string>("MigrateDatabase", bool.FalseString)
                         });
                     });
                 });
