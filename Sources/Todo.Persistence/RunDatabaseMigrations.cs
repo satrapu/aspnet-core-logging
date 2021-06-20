2a90dab9a21f586a3c3736409778c7a01ec0c7de
Bogdan Marian <satrapu@users.noreply.github.com>
2021-06-20 23:54:24 +0300

    Remove unnecessary hosting dependency

--- Sources/Todo.Persistence/RunDatabaseMigrations.cs
+++ Sources/Todo.Persistence/RunDatabaseMigrations.cs
@@ -7,7 +7,6 @@ namespace Todo.Persistence
 
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
 
     /// <summary>
@@ -17,21 +16,15 @@ namespace Todo.Persistence
     {
         private readonly TodoDbContext todoDbContext;
         private readonly IConfiguration configuration;
-        private readonly IHostApplicationLifetime hostApplicationLifetime;
         private readonly ILogger logger;
 
         public RunDatabaseMigrations(
             TodoDbContext todoDbContext,
             IConfiguration configuration,
-            IHostApplicationLifetime hostApplicationLifetime,
             ILogger<RunDatabaseMigrations> logger)
         {
             this.todoDbContext = todoDbContext ?? throw new ArgumentNullException(nameof(todoDbContext));
             this.configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
-
-            this.hostApplicationLifetime = hostApplicationLifetime ??
-                                           throw new ArgumentNullException(nameof(hostApplicationLifetime));
-
             this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
@@ -73,10 +66,8 @@ namespace Todo.Persistence
             }
             catch (Exception exception)
             {
-                logger.LogCritical(exception,
-                    "Failed to migrate database {DatabaseName}; application will stop immediately", database);
-
-                hostApplicationLifetime.StopApplication();
+                logger.LogCritical(exception, "Failed to migrate database {DatabaseName}", database);
+                throw;
             }
         }
     }
