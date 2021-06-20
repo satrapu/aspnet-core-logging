8ca72e877651977ac35956d51fc73cd1720ad1ab
Bogdan Marian <satrapu@users.noreply.github.com>
2021-06-21 00:24:25 +0300

    Add missing XML comments

--- Sources/Todo.Integrations.MiniProfiler/MiniProfilerActivator.cs
+++ Sources/Todo.Integrations.MiniProfiler/MiniProfilerActivator.cs
@@ -3,8 +3,16 @@ namespace Todo.Integrations.MiniProfiler
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
+    /// <summary>
+    /// Contains extension methods applicable to <see cref="IServiceCollection"/> class.
+    /// </summary>
     public static class MiniProfilerActivator
     {
+        /// <summary>
+        /// Adds MiniProfiler related services to the current <paramref name="services"/> instance.
+        /// </summary>
+        /// <param name="services">The application services collection.</param>
+        /// <param name="configuration">The application configuration.</param>
         public static void ActivateMiniProfiler(this IServiceCollection services, IConfiguration configuration)
         {
             // Configure MiniProfiler for Web API and EF Core.
