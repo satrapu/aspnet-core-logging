77e59a441d9caaeb237b21d94267e8c834403a35
Bogdan Marian <satrapu@users.noreply.github.com>
2021-12-03 23:06:15 +0200

    Migrate application from .NET 5 to .NET 6 (#23)

--- Sources/Todo.Persistence/TodoDbContext.cs
+++ Sources/Todo.Persistence/TodoDbContext.cs
@@ -1,5 +1,7 @@
 namespace Todo.Persistence
 {
+    using System;
+
     using Entities;
     using Entities.Configurations;
 
@@ -10,6 +12,11 @@ namespace Todo.Persistence
     {
         public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
         {
+            // satrapu 2021-12-02: Temporarily disable a breaking change introduced when migrating
+            // Npgsql.EntityFrameworkCore.PostgreSQL NuGet package from v5.x to v6.x.
+            // See more about this breaking change and its fix here:
+            // https://www.npgsql.org/efcore/release-notes/6.0.html#opting-out-of-the-new-timestamp-mapping-logic.
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
         }
 
         // ReSharper disable once UnusedAutoPropertyAccessor.Global
