2383328c836e531ed1b220a5a58d516b1d7c3c1e
Bogdan Marian <satrapu@users.noreply.github.com>
2021-12-12 19:03:46 +0200

    Ensure EF Core DB provider uses a switch set once per app domain

--- Sources/Todo.Persistence/TodoDbContext.cs
+++ Sources/Todo.Persistence/TodoDbContext.cs
@@ -10,13 +10,18 @@ namespace Todo.Persistence
     // ReSharper disable once ClassNeverInstantiated.Global
     public class TodoDbContext : DbContext
     {
-        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        static TodoDbContext()
         {
             // satrapu 2021-12-02: Temporarily disable a breaking change introduced when migrating
             // Npgsql.EntityFrameworkCore.PostgreSQL NuGet package from v5.x to v6.x.
             // See more about this breaking change and its fix here:
             // https://www.npgsql.org/efcore/release-notes/6.0.html#opting-out-of-the-new-timestamp-mapping-logic.
             AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public TodoDbContext(DbContextOptions<TodoDbContext> options) : base(options)
+        {
+            
         }
 
         // ReSharper disable once UnusedAutoPropertyAccessor.Global
