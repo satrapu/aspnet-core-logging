cb8f496df9c51fce04ecaae86d6a46f3aedce857
Bogdan Marian <satrapu@users.noreply.github.com>
2021-12-22 10:55:57 +0200

    Improve logging HTTP requests and responses inside tests

--- Tests/Infrastructure/Todo.WebApi.TestInfrastructure/HttpLoggingHandler.cs
+++ Tests/Infrastructure/Todo.WebApi.TestInfrastructure/HttpLoggingHandler.cs
@@ -1,6 +1,7 @@
 namespace Todo.WebApi.TestInfrastructure
 {
     using System.Net.Http;
+    using System.Net.Http.Headers;
     using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
@@ -20,44 +21,74 @@ namespace Todo.WebApi.TestInfrastructure
             this.logger = logger;
         }
 
+        /// <summary>
+        /// This method logs the HTTP request and its corresponding response using the format documented here:
+        /// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages.
+        /// </summary>
+        /// <param name="request">The HTTP request to process.</param>
+        /// <param name="cancellationToken">The token used for canceling processing the given HTTP request.</param>
+        /// <returns>The HTTP response received from the server.</returns>
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
             var stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("-- REQUEST: BEGIN --");
-            stringBuilder.AppendLine($"{request}\nContent:\n\t");
-
-            if (request.Content != null)
-            {
-                stringBuilder.AppendLine(await request.Content.ReadAsStringAsync(cancellationToken));
-            }
-            else
-            {
-                stringBuilder.AppendLine("N/A");
-            }
-
-            stringBuilder.AppendLine("-- REQUEST: END --");
-
+            await stringBuilder.AppendAsync(request, cancellationToken);
+            stringBuilder.AppendLine();
             HttpResponseMessage response = await base.SendAsync(request, cancellationToken);
-
-            stringBuilder.AppendLine("-- RESPONSE: BEGIN --");
-            stringBuilder.AppendLine($"{response}\nContent:\n\t");
-
-            if (request.Content != null)
-            {
-                stringBuilder.AppendLine(await response.Content.ReadAsStringAsync(cancellationToken));
-            }
-            else
-            {
-                stringBuilder.AppendLine("N/A");
-            }
-
-            stringBuilder.AppendLine("-- RESPONSE: END --");
+            await stringBuilder.AppendAsync(response, cancellationToken);
 
             // ReSharper disable once TemplateIsNotCompileTimeConstantProblem
             logger.LogInformation(stringBuilder.ToString());
 
             return response;
+        }
+    }
+
+    internal static class StringBuilderExtensions
+    {
+        public static async Task AppendAsync(this StringBuilder stringBuilder, HttpRequestMessage request,
+        CancellationToken cancellationToken)
+        {
+            stringBuilder.AppendLine("-- REQUEST: BEGIN --");
+            stringBuilder.AppendLine($"{request.Method.Method} {request.RequestUri.LocalPath} HTTP/{request.Version}");
+            stringBuilder.AppendHeaders(request.Headers);
+            await stringBuilder.AppendAsync(request.Content, cancellationToken);
+            stringBuilder.AppendLine("-- REQUEST: END --");
+        }
+
+        public static async Task AppendAsync(this StringBuilder stringBuilder, HttpResponseMessage response,
+            CancellationToken cancellationToken)
+        {
+            stringBuilder.AppendLine("-- RESPONSE: BEGIN --");
+            stringBuilder.AppendLine($"HTTP/{response.Version} {(int) response.StatusCode} {response.StatusCode}");
+            stringBuilder.AppendHeaders(response.Headers);
+            await stringBuilder.AppendAsync(response.Content, cancellationToken);
+            stringBuilder.AppendLine("-- RESPONSE: END --");
+        }
+
+        private static void AppendHeaders(this StringBuilder stringBuilder, HttpHeaders headers)
+        {
+            if (headers == null)
+            {
+                return;
+            }
+
+            foreach (var header in headers)
+            {
+                stringBuilder.AppendLine($"{header.Key}: {string.Join(", ", header.Value)}");
+            }
+        }
+
+        private static async Task AppendAsync(this StringBuilder stringBuilder, HttpContent content,
+            CancellationToken cancellationToken)
+        {
+            if (content == null)
+            {
+                return;
+            }
+
+            string contentAsString = await content.ReadAsStringAsync(cancellationToken);
+            stringBuilder.AppendLine($"\n{contentAsString}");
         }
     }
 }
