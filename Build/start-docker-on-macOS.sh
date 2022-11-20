8e7f50c23592eb642ac22001b207d5fd05ebc331
Bogdan Marian <satrapu@users.noreply.github.com>
2022-11-20 23:00:25 +0200

    Use the official guide for installing Docker Desktop for Mac via CLI, take 3

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -11,6 +11,10 @@ set -o nounset
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577574.
 set -o errexit
 
+echo 'Listing all users ...'
+ls /Users
+echo 'Users have been listed'
+
 echo 'Downloading Docker Desktop for Mac installation file ...'
 start=$SECONDS
 curl -O -L https://desktop.docker.com/mac/main/amd64/Docker.dmg
@@ -20,12 +24,37 @@ echo "Installation file has been downloaded in $duration seconds"
 
 echo 'Installing Docker Desktop for Mac ...'
 start=$SECONDS
+userNameForRunningDocker=runner
 sudo hdiutil attach Docker.dmg
-sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --user=runner --accept-license
+sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --user=$userNameForRunningDocker --accept-license
 sudo hdiutil detach /Volumes/Docker
 end=$SECONDS
 duration=$(( end - start ))
 echo "Docker Desktop for Mac has been installed in $duration seconds"
 
-docker --version
-docker compose --version
+echo 'Starting Docker service ...'
+start=$SECONDS
+
+retries=0
+maxRetries=30
+
+while ! docker info 2>/dev/null ; do
+    sleep 5s
+    ((retries=retries+1))
+
+    if [[ ${retries} -gt ${maxRetries} ]]; then
+        >&2 echo 'Docker service failed to enter running state during the expected time'
+        exit 1
+    fi;
+
+    echo 'Waiting for Docker service to enter running state ...'
+done
+
+end=$SECONDS
+duration=$(( end - start ))
+echo "Docker service has started after $duration seconds"
+
+echo 'Displaying Docker & Docker Compose versions ...'
+docker version
+docker compose version
+echo 'All good - start running some containers on this machine'
