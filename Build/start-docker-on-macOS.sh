0b5703a76695a4ca3d3148e55efde169cb3e1e03
Bogdan Marian <satrapu@users.noreply.github.com>
2022-11-20 22:01:19 +0200

    Install Docker Desktop for Mac using native CLI support

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Based on: http://web.archive.org/web/20201012054023if_/https://github.com/microsoft/azure-pipelines-image-generation/issues/738#issuecomment-522301481.
-# The original GitHub issue link is no longer available, thus I had to resort to the Wayback Machine URL!
+# Install Docker Desktop for Mac via brew tool.
+# This script is based on: https://github.com/docker/for-mac/issues/2359#issuecomment-991942550.
 
 # Fail script in case of unset variables - see more here:
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577541.
@@ -11,57 +11,20 @@ set -o nounset
 # http://web.archive.org/web/20110314180918/http://www.davidpashley.com/articles/writing-robust-shell-scripts.html#id2577574.
 set -o errexit
 
-# Install Docker Desktop for Mac via brew tool
-echo 'Downloading and then running docker brew formula ...'
-start=$SECONDS
-
-# The brew formula below will install Docker Desktop for Mac, v4.14.1,91661.
-dockerGitCommitSha='df83ad35fd73df03fb423f29db732abb4495e172'
-dockerInstallationScriptName='docker.rb'
-dockerInstallationScriptUrl="https://raw.githubusercontent.com/Homebrew/homebrew-cask/$dockerGitCommitSha/Casks/$dockerInstallationScriptName"
-curl -L  $dockerInstallationScriptUrl > $dockerInstallationScriptName && brew install $dockerInstallationScriptName
-
-end=$SECONDS
-duration=$(( end - start ))
-echo "Docker brew formula has been downloaded & run in $duration seconds"
-
 echo 'Installing Docker Desktop for Mac ...'
 start=$SECONDS
-sudo /Applications/Docker.app/Contents/MacOS/Docker --quit-after-install --unattended
-/Applications/Docker.app/Contents/MacOS/Docker --unattended &
+
+brew install --cask docker
+docker_app_path=$(brew list --cask docker | grep '==> App' -A1 | tail -n 1 | awk '{ print $1 }')
+docker_app_path="${docker_app_path/#\~/$HOME}"
+
+sudo "$docker_app_path"/Contents/MacOS/Docker --unattended --install-privileged-components
+open -a "$docker_app_path" --args --unattended --accept-license
+while ! "$docker_app_path"/Contents/Resources/bin/docker info &>/dev/null; do sleep 1; done
 
 end=$SECONDS
 duration=$(( end - start ))
-echo "Docker Desktop for Mac has been installed in $duration seconds"
-
-echo 'Starting Docker service ...'
-start=$SECONDS
-
-retries=0
-maxRetries=30
-
-while ! docker info 2>/dev/null ; do
-    sleep 5s
-    ((retries=retries+1))
-
-    if pgrep -xq -- 'Docker'; then
-        echo 'Docker service is still booting'
-    else
-        echo 'Docker service is no longer running, need to restart it'
-        /Applications/Docker.app/Contents/MacOS/Docker --unattended &
-    fi
-
-    if [[ ${retries} -gt ${maxRetries} ]]; then
-        >&2 echo 'Docker service failed to enter running state during the expected time'
-        exit 1
-    fi;
-
-    echo 'Waiting for Docker service to enter running state ...'
-done
-
-end=$SECONDS
-duration=$(( end - start ))
-echo "Docker service has started after $duration seconds"
+echo "Docker Desktop for Mac has been installed & started after $duration seconds"
 
 docker --version
-docker-compose --version
+docker compose --version
