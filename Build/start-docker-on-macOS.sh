d936b7fa9fd6eaa1a4795e64fac544d874801f85
Bogdan Marian <satrapu@users.noreply.github.com>
2022-11-22 22:38:01 +0200

    Use colima the right way

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -6,14 +6,15 @@
 set -o errexit
 set -o nounset
 
-echo 'Installing container runtime ...'
-brew install colima
-echo 'Container runtime has been installed'
+echo 'Starting container runtime ...'
+colima start
+echo 'Container runtime has been started'
 
 echo 'Installing Docker Compose ...'
+# Check for the right Docker Compose version here: https://github.com/docker/compose/releases.
 dockerComposeVersion='2.12.2'
 sudo curl -L https://github.com/docker/compose/releases/download/v$dockerComposeVersion/docker-compose-darwin-aarch64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 echo 'Docker Compose has been installed'
 
 echo 'Checking Docker and Docker Compose installations ...'
