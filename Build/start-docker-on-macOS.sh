c2ae466056fccd810a10458a414797ce98cf36cf
Bogdan Marian <satrapu@users.noreply.github.com>
2022-11-22 22:29:23 +0200

    Install Docker using colima

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -6,8 +6,16 @@
 set -o errexit
 set -o nounset
 
-echo 'Installing Docker ...'
-brew install --cask docker
+echo 'Installing container runtime ...'
+brew install colima
+echo 'Container runtime has been installed'
+
+echo 'Installing Docker Compose ...'
+dockerComposeVersion='2.12.2'
+sudo curl -L https://github.com/docker/compose/releases/download/v$dockerComposeVersion/docker-compose-darwin-aarch64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+echo 'Docker Compose has been installed'
+
+echo 'Checking Docker and Docker Compose installations ...'
 docker info
 docker compose info
-echo 'Docker has been installed'
