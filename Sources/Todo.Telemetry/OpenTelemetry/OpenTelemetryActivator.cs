a11fc556fe3aabae6b29d2642b8034a2c15a32e4
Bogdan Marian <satrapu@users.noreply.github.com>
2023-11-19 17:32:03 +0200

    Remove unreachable code fragment

--- Sources/Todo.Telemetry/OpenTelemetry/OpenTelemetryActivator.cs
+++ Sources/Todo.Telemetry/OpenTelemetry/OpenTelemetryActivator.cs
@@ -111,7 +111,7 @@ namespace Todo.Telemetry.OpenTelemetry
                             {
                                 { "service.instance.attributes.custom.EnvironmentName", webHostEnvironment.EnvironmentName },
                                 { "service.instance.attributes.custom.ContentRootPath", webHostEnvironment.ContentRootPath },
-                                { "service.instance.attributes.custom.WebRootPath", webHostEnvironment.WebRootPath ?? "null" },
+                                { "service.instance.attributes.custom.WebRootPath", webHostEnvironment.WebRootPath },
                                 { "service.instance.attributes.custom.OperationSystem", Environment.OSVersion.ToString() },
                                 { "service.instance.attributes.custom.MachineName", Environment.MachineName },
                                 { "service.instance.attributes.custom.ProcessorCount", Environment.ProcessorCount.ToString() },
