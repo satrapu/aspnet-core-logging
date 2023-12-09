05de59be0eeffb1327a137204cdf2fb81abd9fdd
Bogdan Marian <satrapu@users.noreply.github.com>
2023-12-09 19:15:43 +0200

    Add the new connection string needed when running acceptance tests

--- Sources/Todo.Commons/Constants/ConnectionStrings.cs
+++ Sources/Todo.Commons/Constants/ConnectionStrings.cs
@@ -11,6 +11,11 @@ namespace Todo.Commons.Constants
         public const string UsedByIntegrationTests = "TodoForIntegrationTests";
 
         /// <summary>
+        /// Represents the name of the connection string to be used when running acceptance tests.
+        /// </summary>
+        public const string UsedByAcceptanceTests = "TodoForAcceptanceTests";
+
+        /// <summary>
         /// Represents the name of the connection string to be used when running the application.
         /// </summary>
         public const string UsedByApplication = "Todo";
