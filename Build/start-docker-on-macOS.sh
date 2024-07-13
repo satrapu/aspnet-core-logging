3b2513930745d26be5149b13af93e557f17779d7
Bogdan Marian <satrapu@users.noreply.github.com>
2024-07-13 12:50:17 +0300

    Install specific Homebrew formula version of Docker CLI

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -7,7 +7,6 @@ set -o errexit
 set -o nounset
 
 # Check for the right Docker CLI version here: https://github.com/docker/cli/tags.
-# Installation steps can be found here: https://get.docker.com/.
 dockerCliVersion='27.0.3'
 
 # Check for the right Docker Compose version here: https://github.com/docker/compose/releases.
@@ -15,7 +14,11 @@ dockerCliVersion='27.0.3'
 dockerComposeVersion='2.28.1'
 
 echo "Installing Docker CLI with version: $dockerCliVersion ..."
-brew install docker@$dockerCliVersion
+# Check for installing a specific Homebrew formula version here: https://blog.sandipb.net/2021/09/02/installing-a-specific-version-of-a-homebrew-formula/.
+localDockerCliHomeFolder=$USER/local-docker-cli
+brew tap-new "$localDockerCliHomeFolder"
+brew extract --version=$dockerCliVersion docker "$localDockerCliHomeFolder"
+HOMEBREW_NO_AUTO_UPDATE=1 brew install "$localDockerCliHomeFolder@$dockerCliVersion"
 echo 'Checking Docker installation ...'
 docker --version
 echo "Docker CLI v$dockerCliVersion has been installed successfully"
