9292145e4a56474252df1ee04557a3576110e64a
Bogdan Marian <satrapu@users.noreply.github.com>
2024-07-13 14:05:14 +0300

    Install latest version of Docker CLI (for now)

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -6,30 +6,22 @@
 set -o errexit
 set -o nounset
 
-# Check for the right Docker CLI version here: https://github.com/docker/cli/tags.
-dockerCliVersion='27.0.3'
-
 # Check for the right Docker Compose version here: https://github.com/docker/compose/releases.
 # Installation steps can be found here: https://docs.docker.com/compose/install/standalone/.
 dockerComposeVersion='2.28.1'
 
-echo "Installing Docker CLI with version: $dockerCliVersion ..."
-# Check for installing a specific Homebrew formula version here: https://github.com/orgs/Homebrew/discussions/155#discussioncomment-133894.
-dockerCustomFormula="$USER/custom-docker"
-brew uninstall docker
-brew tap-new "$dockerCustomFormula"
-brew extract --version $dockerCliVersion docker "$dockerCustomFormula"
-brew install "docker@$dockerCliVersion"
+echo "Installing Docker CLI ..."
+brew install docker
 echo 'Checking Docker CLI installation ...'
 docker --version
-echo "Docker CLI v$dockerCliVersion has been installed successfully"
+echo "Docker CLI has been installed successfully"
 
 echo "Installing Docker Compose with version: $dockerComposeVersion ..."
 sudo curl -L https://github.com/docker/compose/releases/download/v$dockerComposeVersion/docker-compose-darwin-x86_64 -o /usr/local/bin/docker-compose -v
 sudo chmod +x /usr/local/bin/docker-compose
 echo 'Checking Docker Compose installation ...'
 docker-compose version
-echo "Docker Compose v$dockerComposeVersion has been installed successfully"
+echo "Docker Compose has been installed successfully"
 
 # Colima container runtime usage can be found here: https://github.com/abiosoft/colima?tab=readme-ov-file#usage.
 echo 'Starting Colima container runtime ...'
