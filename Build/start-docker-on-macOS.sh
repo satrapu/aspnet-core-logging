ded7f11fa4cad8b19b9bcc84edb0fc7a4db5a597
Bogdan Marian <satrapu@users.noreply.github.com>
2024-07-13 18:42:03 +0300

    Customize Colima VM

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -10,12 +10,19 @@ set -o nounset
 # Installation steps can be found here: https://docs.docker.com/compose/install/standalone/.
 dockerComposeVersion='2.28.1'
 
+# Check for how to customize Colima VM here: https://github.com/abiosoft/colima?tab=readme-ov-file#customizing-the-vm.
+colimaCpuCount=2
+colimaMemorySizeInGigabytes=2
+colimaDiskSizeInGigabytes=10
+
+# Install Docker CLI
 echo "Installing Docker CLI ..."
 brew install docker
 echo 'Checking Docker CLI installation ...'
 docker --version
 echo "Docker CLI has been installed successfully"
 
+# Install Docker Compose
 echo "Installing Docker Compose with version: $dockerComposeVersion ..."
 sudo curl -L https://github.com/docker/compose/releases/download/v$dockerComposeVersion/docker-compose-darwin-x86_64 -o /usr/local/bin/docker-compose -v
 sudo chmod +x /usr/local/bin/docker-compose
@@ -23,8 +30,9 @@ echo 'Checking Docker Compose installation ...'
 docker-compose version
 echo "Docker Compose has been installed successfully"
 
-# Colima container runtime usage can be found here: https://github.com/abiosoft/colima?tab=readme-ov-file#usage.
+# Start Colima container runtime.
+# Check for Colima usage here: https://github.com/abiosoft/colima?tab=readme-ov-file#usage.
 echo 'Starting Colima container runtime ...'
-colima start
+colima start --cpu $colimaCpuCount --memory $colimaMemorySizeInGigabytes --disk $colimaDiskSizeInGigabytes
 echo 'Colima container runtime has started'
 echo 'All good :)'
