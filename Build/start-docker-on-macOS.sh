ce01affb86a642ac40bc5cf7175b78e5a6d2f8c7
Bogdan Marian <satrapu@users.noreply.github.com>
2024-07-13 12:08:22 +0300

    Make the script more developer friendly

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -6,25 +6,30 @@
 set -o errexit
 set -o nounset
 
-echo 'Installing Docker client ...'
-brew install docker
-echo 'Docker client has been installed'
+# Check for the right Docker CLI version here: https://github.com/docker/cli/tags.
+# Installation steps can be found here: https://get.docker.com/.
+dockerCliVersion='27.0.3'
 
-echo 'Installing Docker Compose ...'
 # Check for the right Docker Compose version here: https://github.com/docker/compose/releases.
+# Installation steps can be found here: https://docs.docker.com/compose/install/standalone/.
 dockerComposeVersion='2.28.1'
-sudo curl -L https://github.com/docker/compose/releases/download/v$dockerComposeVersion/docker-compose-darwin-x86_64 -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-echo 'Docker Compose has been installed'
 
-echo 'Starting container runtime ...'
-colima start
-echo 'Container runtime has started'
-
+echo "Installing Docker CLI with version: $dockerCliVersion ..."
+curl -fsSL https://get.docker.com -o install-docker.sh -v
+sh install-docker.sh --version $dockerCliVersion
 echo 'Checking Docker installation ...'
-docker info
+docker --version
+echo "Docker CLI v$dockerCliVersion has been installed successfully"
 
+echo "Installing Docker Compose with version: $dockerComposeVersion ..."
+sudo curl -L https://github.com/docker/compose/releases/download/v$dockerComposeVersion/docker-compose-darwin-x86_64 -o /usr/local/bin/docker-compose -v
+sudo chmod +x /usr/local/bin/docker-compose
 echo 'Checking Docker Compose installation ...'
 docker-compose version
+echo "Docker Compose v$dockerComposeVersion has been installed successfully"
 
+# Colima container runtime usage can be found here: https://github.com/abiosoft/colima?tab=readme-ov-file#usage.
+echo 'Starting Colima container runtime ...'
+colima start
+echo 'Colima container runtime has started'
 echo 'All good :)'
