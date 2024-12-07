ce6aa75162de91c82ab417f03af1b404ac5d8440
Bogdan Marian <satrapu@users.noreply.github.com>
2024-12-07 15:46:41 +0200

    Simplify assertions by using Verify.NUnit lib instead

--- Tests/IntegrationTests/Todo.WebApi.IntegrationTests/ExceptionHandling/CustomExceptionHandlerTests.cs
+++ Tests/IntegrationTests/Todo.WebApi.IntegrationTests/ExceptionHandling/CustomExceptionHandlerTests.cs
@@ -2,11 +2,8 @@ namespace Todo.WebApi.ExceptionHandling
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
-    using System.Net;
     using System.Net.Http;
     using System.Security.Principal;
-    using System.Text.Json;
     using System.Threading.Tasks;
 
     using ApplicationFlows.Security;
@@ -15,9 +12,6 @@ namespace Todo.WebApi.ExceptionHandling
 
     using Commons.Constants;
 
-    using FluentAssertions;
-    using FluentAssertions.Execution;
-
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
@@ -25,6 +19,8 @@ namespace Todo.WebApi.ExceptionHandling
     using Models;
 
     using NUnit.Framework;
+
+    using VerifyNUnit;
 
     using Services.Security;
 
@@ -44,12 +40,6 @@ namespace Todo.WebApi.ExceptionHandling
         public async Task HandleException_WhenApiThrowsException_MustConvertExceptionToInstanceOfProblemDetailsClass()
         {
             // Arrange
-            var generateJwtModel = new GenerateJwtModel
-            {
-                UserName = $"test-user--{Guid.NewGuid():N}",
-                Password = $"test-password--{Guid.NewGuid():N}"
-            };
-
             await using TestWebApplicationFactory webApplicationFactory = await TestWebApplicationFactory.CreateAsync
             (
                 applicationName: nameof(CustomExceptionHandlerTests),
@@ -82,33 +72,19 @@ namespace Todo.WebApi.ExceptionHandling
                     });
                 });
 
+            GenerateJwtModel generateJwtModel = new()
+            {
+                UserName = $"test-user--{Guid.NewGuid():N}",
+                Password = $"test-password--{Guid.NewGuid():N}"
+            };
+
             using HttpClient httpClient = webApplicationFactory.CreateClient();
 
             // Act
             HttpResponseMessage httpResponseMessage = await httpClient.PostAsJsonAsync("api/jwt", generateJwtModel);
 
             // Assert
-            using AssertionScope _ = new();
-            const HttpStatusCode expectedStatusCode = HttpStatusCode.InternalServerError;
-
-            httpResponseMessage.IsSuccessStatusCode.Should().BeFalse("the endpoint was supposed to throw a hard-coded exception");
-            httpResponseMessage.StatusCode.Should().Be(expectedStatusCode, $"the hard-coded exception was mapped to HTTP status {expectedStatusCode}");
-
-            byte[] problemDetailsAsBytes = await httpResponseMessage.Content.ReadAsByteArrayAsync();
-            await using var memoryStream = new MemoryStream(problemDetailsAsBytes);
-
-            // Must use System.Text.Json.JsonSerializer instead of Newtonsoft.Json.JsonSerializer to ensure
-            // ProblemDetails.Extensions property is correctly deserialized and does not end up as an empty
-            // dictionary.
-            ProblemDetails problemDetails = await JsonSerializer.DeserializeAsync<ProblemDetails>(memoryStream);
-            problemDetails.Should().NotBeNull("application must handle any exception");
-            // ReSharper disable once PossibleNullReferenceException
-            problemDetails.Extensions.Should().NotBeNull("problem details must contain extra info");
-            problemDetails.Extensions.Should().NotContainKey("errorData", "error data must not be present");
-            problemDetails.Extensions.Should().ContainKey("rootCauseKey", "root cause key must be present");
-            problemDetails.Extensions.Should().ContainKey("errorId", "error id must be present");
-
-            Guid.TryParse(problemDetails.Extensions["errorId"].ToString(), out Guid _).Should().BeTrue("error id is a GUID");
+            await Verifier.Verify(httpResponseMessage, settings: ModuleInitializer.VerifySettings);
         }
 
         /// <summary>
