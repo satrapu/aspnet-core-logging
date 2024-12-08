08134e38c2e49d50bab41191dd361b404679910e
Bogdan Marian <satrapu@users.noreply.github.com>
2024-12-08 19:48:30 +0200

    Ignore code coverage for EF Core related autogenerated class

---
+++ Sources/Todo.Persistence/Migrations/TodoDbContextModelSnapshot.CustomAttributes.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Persistence.Migrations
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    [ExcludeFromCodeCoverage]
+    public partial class TodoDbContextModelSnapshot
+    {
+    }
+}
