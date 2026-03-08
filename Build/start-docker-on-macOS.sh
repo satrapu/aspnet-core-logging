d6d885f2e369e70bc613716393bfcec37297fb9a
Bogdan Marian <satrapu@users.noreply.github.com>
2026-03-08 20:09:29 +0200

    Install container related tools using specific versions

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -6,9 +6,19 @@
 set -o errexit
 set -o nounset
 
+# Check for the right Docker CLI version here: https://github.com/docker/cli/tags.
+DOCKER_CLI_VERSION=29.3.0
+DOCKER_CLI_ARCH="x86_64"
+DOCKER_CLI_DOWNLOAD_BASE_URL="https://download.docker.com/mac/static/stable/$DOCKER_CLI_ARCH"
+DOCKER_CLI_TAR_FILE="docker-${DOCKER_VERSION}.tgz"
+DOCKER_CLI_INSTALL_TMP_DIR="$(mktemp -d)"
+
 # Check for the right Docker Compose version here: https://github.com/docker/compose/releases.
 # Installation steps can be found here: https://docs.docker.com/compose/install/standalone/.
-dockerComposeVersion='5.0.2'
+DOCKER_COMPOSE_VERSION='5.1.0'
+
+# Check for the right Colima version here: https://github.com/abiosoft/colima/releases.
+COLIMA_VERSION=0.10.1
 
 # Check for how to customize Colima VM here: https://github.com/abiosoft/colima?tab=readme-ov-file#customizing-the-vm.
 colimaCpuCount=2
@@ -16,22 +26,31 @@ colimaMemorySizeInGigabytes=2
 colimaDiskSizeInGigabytes=10
 
 # Install Docker CLI
-echo "Installing Docker CLI ..."
-brew install docker
+echo "Installing Docker CLI with version: $DOCKER_CLI_VERSION ..."
+curl -L "$DOCKER_CLI_DOWNLOAD_BASE_URL/$DOCKER_CLI_TAR_FILE" -o "$DOCKER_CLI_INSTALL_TMP_DIR/$DOCKER_CLI_TAR_FILE"
+echo "Extracting Docker CLI archive ..."
+tar -xzf "$DOCKER_CLI_INSTALL_TMP_DIR/$DOCKER_CLI_TAR_FILE" -C "$DOCKER_CLI_INSTALL_TMP_DIR"
+sudo mv "$DOCKER_CLI_INSTALL_TMP_DIR/docker/docker" /usr/local/bin/docker
+sudo chmod +x /usr/local/bin/docker
 echo 'Checking Docker CLI installation ...'
 docker --version
 echo "Docker CLI has been installed successfully"
 
 # Install Docker Compose
-echo "Installing Docker Compose with version: $dockerComposeVersion ..."
-sudo curl -L https://github.com/docker/compose/releases/download/v$dockerComposeVersion/docker-compose-darwin-x86_64 -o /usr/local/bin/docker-compose -v
+echo "Installing Docker Compose with version: $DOCKER_COMPOSE_VERSION ..."
+sudo curl -L https://github.com/docker/compose/releases/download/v$DOCKER_COMPOSE_VERSION/docker-compose-darwin-x86_64 -o /usr/local/bin/docker-compose -v
 sudo chmod +x /usr/local/bin/docker-compose
 echo 'Checking Docker Compose installation ...'
 docker-compose version
 echo "Docker Compose has been installed successfully"
 
 # Install Colima
-brew install colima
+echo "Installing Colima with version: $COLIMA_VERSION ..."
+curl -L "https://github.com/abiosoft/colima/releases/download/v${COLIMA_VERSION}/colima-$(uname -m)" -o /usr/local/bin/colima
+chmod +x /usr/local/bin/colima
+echo 'Checking Colima installation ...'
+colima version
+echo "Colima has been installed successfully"
 
 # Start Colima container runtime.
 # Check for Colima usage here: https://github.com/abiosoft/colima?tab=readme-ov-file#usage.
