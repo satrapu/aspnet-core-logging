e0d8f955aa75371864ac848835a7d67f65a13f55
Bogdan Marian <satrapu@users.noreply.github.com>
2026-03-14 17:19:01 +0200

    Refactor Docker and Lima installation scripts, take 2

--- Build/start-docker-on-macOS.sh
+++ Build/start-docker-on-macOS.sh
@@ -6,6 +6,14 @@
 set -o errexit
 set -o nounset
 
+export PATH="/usr/local/bin:$PATH"
+
+# Ensure Lima does not run.
+limactl stop || true
+
+# Ensure Colima does not run.
+colima stop || true
+
 # Check for the right Docker CLI version here: https://github.com/docker/cli/tags.
 DOCKER_CLI_VERSION="29.3.0"
 DOCKER_CLI_ARCH="x86_64"
@@ -31,18 +39,18 @@ COLIMA_ARCH="x86_64"
 COLIMA_OS_NAME="Darwin"
 
 # Check for how to customize Colima VM here: https://github.com/abiosoft/colima?tab=readme-ov-file#customizing-the-vm.
-colimaCpuCount=2
-colimaMemorySizeInGigabytes=2
-colimaDiskSizeInGigabytes=10
+COLIMA_CPU_COUNT=2
+COLIMA_MEMORY_SIZE_IN_GIGABYTES=2
+COLIMA_DISK_SIZE_IN_GIGABYTES=5
 
 # Install Docker CLI.
 echo "Installing Docker CLI with version: ${DOCKER_CLI_VERSION} ..."
 echo "Downloading Docker CLI archive ..."
-sudo curl -L "${DOCKER_CLI_DOWNLOAD_BASE_URL}/${DOCKER_CLI_TAR_FILE}" -o "${DOCKER_CLI_INSTALL_DIR}/${DOCKER_CLI_TAR_FILE}"
+curl -L "${DOCKER_CLI_DOWNLOAD_BASE_URL}/${DOCKER_CLI_TAR_FILE}" -o "${DOCKER_CLI_INSTALL_DIR}/${DOCKER_CLI_TAR_FILE}"
 echo "Extracting Docker CLI archive ..."
 tar -xzf "${DOCKER_CLI_INSTALL_DIR}/${DOCKER_CLI_TAR_FILE}" --strip-components=1 -C "${DOCKER_CLI_INSTALL_DIR}"
-sudo mv "${DOCKER_CLI_INSTALL_DIR}/docker" /usr/local/bin/docker
-sudo chmod +x /usr/local/bin/docker
+mv "${DOCKER_CLI_INSTALL_DIR}/docker" /usr/local/bin/docker
+chmod +x /usr/local/bin/docker
 echo 'Checking Docker CLI installation ...'
 docker --version
 echo "Docker CLI has been installed successfully"
@@ -51,9 +59,9 @@ printf "\n\n\n"
 # Install Docker Compose.
 echo "Installing Docker Compose with version: ${DOCKER_COMPOSE_VERSION} ..."
 echo "Downloading Docker Compose archive ..."
-sudo curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-${DOCKER_COMPOSE_OS_NAME}-${DOCKER_COMPOSE_ARCH}" \
+curl -L "https://github.com/docker/compose/releases/download/v${DOCKER_COMPOSE_VERSION}/docker-compose-${DOCKER_COMPOSE_OS_NAME}-${DOCKER_COMPOSE_ARCH}" \
           -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 echo 'Checking Docker Compose installation ...'
 docker-compose version
 echo "Docker Compose has been installed successfully"
@@ -62,30 +70,24 @@ printf "\n\n\n"
 # Install Lima (required for Colima).
 echo "Installing Lima with version: ${LIMA_VERSION} ..."
 echo "Downloading Lima archive ..."
-sudo curl -L "https://github.com/lima-vm/lima/releases/download/v${LIMA_VERSION}/lima-${LIMA_VERSION}-${LIMA_OS_NAME}-${LIMA_ARCH}.tar.gz" \
+curl -L "https://github.com/lima-vm/lima/releases/download/v${LIMA_VERSION}/lima-${LIMA_VERSION}-${LIMA_OS_NAME}-${LIMA_ARCH}.tar.gz" \
           -o "${LIMA_INSTALL_DIR}/lima.tar.gz"
 echo "Extracting Lima archive ..."
-sudo tar -xzf "${LIMA_INSTALL_DIR}/lima.tar.gz" --strip-components=1 -C "${LIMA_INSTALL_DIR}"
-sudo mv "${LIMA_INSTALL_DIR}/bin/limactl" /usr/local/bin/limactl
-sudo cp -r "${LIMA_INSTALL_DIR}/share/lima" /usr/local/share/lima
-sudo chmod +x /usr/local/bin/limactl
+tar -xzf "${LIMA_INSTALL_DIR}/lima.tar.gz" --strip-components=1 -C "${LIMA_INSTALL_DIR}"
+mv "${LIMA_INSTALL_DIR}/bin/limactl" /usr/local/bin/limactl
+cp -r "${LIMA_INSTALL_DIR}/share/lima" /usr/local/share/lima
+chmod +x /usr/local/bin/limactl
 echo 'Checking Lima installation ...'
 limactl --version
 echo "Lima has been installed successfully"
 printf "\n\n\n"
 
-# Start Lima (required before using Colima).
-echo "Starting Lima ..."
-limactl start
-echo "Lima has been started successfully"
-printf "\n\n\n"
-
 # Install Colima.
 echo "Installing Colima with version: ${COLIMA_VERSION} ..."
 echo "Downloading Colima archive ..."
-sudo curl -L "https://github.com/abiosoft/colima/releases/download/v${COLIMA_VERSION}/colima-${COLIMA_OS_NAME}-${COLIMA_ARCH}" \
+curl -L "https://github.com/abiosoft/colima/releases/download/v${COLIMA_VERSION}/colima-${COLIMA_OS_NAME}-${COLIMA_ARCH}" \
           -o /usr/local/bin/colima
-sudo chmod +x /usr/local/bin/colima
+chmod +x /usr/local/bin/colima
 echo 'Checking Colima installation ...'
 colima version
 echo "Colima has been installed successfully"
@@ -94,7 +96,7 @@ printf "\n\n\n"
 # Start Colima.
 # Check for Colima usage here: https://github.com/abiosoft/colima?tab=readme-ov-file#usage.
 echo 'Starting Colima container runtime ...'
-colima start --cpu $colimaCpuCount --memory $colimaMemorySizeInGigabytes --disk $colimaDiskSizeInGigabytes
+colima start --cpu $COLIMA_CPU_COUNT --memory $COLIMA_MEMORY_SIZE_IN_GIGABYTES --disk $COLIMA_DISK_SIZE_IN_GIGABYTES
 echo 'Colima container runtime has started'
 printf "\n\n\n"
 echo 'All good :)'
