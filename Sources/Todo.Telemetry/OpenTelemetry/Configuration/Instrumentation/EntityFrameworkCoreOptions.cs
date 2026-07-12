fe01f377740bce27a1da90e89e132a8b977c4960
satrapu <satrapu@users.noreply.github.com>
2026-07-12 16:53:56 +0300

    Migrate solution to .NET 10

--- Sources/Todo.Telemetry/OpenTelemetry/Configuration/Instrumentation/EntityFrameworkCoreOptions.cs
+++
@@ -1,7 +0,0 @@
-namespace Todo.Telemetry.OpenTelemetry.Configuration.Instrumentation
-{
-    public class EntityFrameworkCoreOptions
-    {
-        public bool SetDbStatementForText { get; set; }
-    }
-}
